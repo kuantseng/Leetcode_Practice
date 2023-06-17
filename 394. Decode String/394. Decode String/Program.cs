02f8215352de6213202da40a88d55321577f717a
Kaleb <tseng.huaikuan@gmail.com>
2023-06-17 23:03:30 +0800

    Update_All_Practice

---
+++ 394. Decode String/394. Decode String/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _394._Decode_String
+{
+    class Program
+    {
+        public static string Encode_Str = "3[a]2[bc[2bc2[bc]]]";
+        public static string Result;
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Str_Decode(Encode_Str));
+            Console.ReadKey();
+        }
+        public static string Str_Decode(string input)
+        {
+            Result = input;
+            while (Result.IndexOf("[") > 0)
+            {
+                int coefficient = 0;   // coefficient
+                int temp_coeff = 0;  
+                int pre_char_count = 1;
+                string temp_result_str = "";
+                int start_symbol = Result.LastIndexOf("[");
+                string temp_str = Result.Substring(start_symbol, Result.Length - start_symbol);
+                int end_symbol = Result.IndexOf("]", start_symbol);
+                temp_str = Result.Substring(start_symbol+1, end_symbol-start_symbol-1);
+                try
+                {
+                    while ((int.TryParse(Result.Substring(start_symbol - pre_char_count, pre_char_count), out temp_coeff)))
+                    {
+                        pre_char_count++;
+                        coefficient = temp_coeff;
+                    }
+                }
+                catch
+                {
+
+                }
+
+                for (int i = 0; i < coefficient; i++)
+                {
+                    temp_result_str = temp_result_str + temp_str;
+                }
+
+                Result = Result.Substring(0,start_symbol - (pre_char_count-1)) + temp_result_str + Result.Substring(end_symbol+1);
+            }
+            return Result;
+        }
+    }
+}
---
+++ 654. Maximum Binary Tree/654. Maximum Binary Tree/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _654._Maximum_Binary_Tree
+{
+    class Program
+    {
+        public static int[] input;
+        public static List<string> Output = new List<string>();
+        public static bool GetResult = false;
+        public static int[] Left_New_List;
+        public static int[] Right_New_List;
+        public static int Min_Val;
+        static void Main(string[] args)
+        {
+            input = new int[] { 3, 2, 1, 6, 0, 5 };
+            Min_Val = input.Min();
+            Method1_FindMax(new List<int[]> { input });  //Method1
+            Output.ForEach(val => Console.WriteLine(val));
+            //nums = Output.Select(int.Parse).ToArray();
+            Console.ReadLine();
+
+        }
+
+        static void Method1_FindMax(List<int[]> array_list)    //Start from the Max Value
+        {
+            GetResult = true;   //Set true for every loop, if array not null, turn false
+            int Temp_Max_Idx;
+            List<int[]> temp_array = new List<int[]>();
+            //int Temp_Max_Val;
+            //int[] Temp_Array;
+            foreach (int[] array in array_list)
+            {
+                if (array.Length != 0)
+                {
+                    Temp_Max_Idx = Array.IndexOf(array, array.Max());    // Find Max Value and it's Addr in array
+                    try // Find left right value and add to output
+                    {
+                        Output.Add(array.Max().ToString()); //Add the max to output
+                        Left_New_List = array.Where(val => Array.IndexOf(array, val) < Temp_Max_Idx).ToArray(); // 3, 2, 1, 6, 0, 5 
+                        Right_New_List = array.Where(val => Array.IndexOf(array, val) > Temp_Max_Idx).ToArray();
+
+                        if (Left_New_List.Length != 0 || Right_New_List.Length != 0)
+                        {
+                            temp_array.Add(Left_New_List);
+                            temp_array.Add(Right_New_List);
+                        }
+
+
+                    }
+                    catch   //if catch, means there is no element in 
+                    {
+                        //No need
+                    }
+                    GetResult = false;
+                    //if (array.Max() == Min_Val) return;
+                }
+                else
+                {
+                        Output.Add("Null");
+                }
+
+            }
+            if (GetResult == true) return;
+            Method1_FindMax(temp_array);     //Left,Right Recursion
+        }
+    }
+}
